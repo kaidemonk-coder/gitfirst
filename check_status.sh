cb35db0cbb928ed675ed8f708a997e2223800dcb
Kailash Subeesh <kaidemonk@gmail.com>
2026-09-18 00:04:37 +0800

    fixed google cloud status to running

--- check_status.sh
+++ check_status.sh
@@ -2,5 +2,5 @@
 echo "=== CLOUD PROVIDER STATUS CHECKER ==="
 echo "AWS EC2:       [ RUNNING ]"
 echo "Azure Compute:  [ RUNNING ]"
-echo "Google Cloud:   [ MAINTENANCE ]"
+echo "Google Cloud:   [ RUNNING ]"
 echo "====================================="
