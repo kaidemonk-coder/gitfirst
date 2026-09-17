59413bbef4e66e6da9217c40477bcd5577cfd20a
Kailash Subeesh <kaidemonk@gmail.com>
2026-09-17 23:50:58 +0800

    added my first cloud status script

---
+++ check_status.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "=== CLOUD PROVIDER STATUS CHECKER ==="
+echo "AWS EC2:       [ RUNNING ]"
+echo "Azure Compute:  [ RUNNING ]"
+echo "Google Cloud:   [ MAINTENANCE ]"
+echo "====================================="
